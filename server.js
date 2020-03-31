31d8ee8f98be57dad6111351c1f979907b7c0aac
mohammed <msaidm@windowslive.com>
2020-03-31 20:16:30 +0200

    Working Webhooks

--- server.js
+++ server.js
@@ -23,44 +23,44 @@ app.get('*', function (req, res) {
 
 
 // WEBHOOK ENDPOINT
-// app.post('/webhook', async function (req, res) {
-//     try {
-//         console.log("got webhook" + req + "   type: " + req.body.message_type);
-//         if (req.body.message_type === 'new_connection') {
-//                     console.log("new connection notif");
+app.post('/webhook', async function (req, res) {
+    try {
+        console.log("got webhook" + req + "   type: " + req.body.message_type);
+        if (req.body.message_type === 'new_connection') {
+                    console.log("new connection notif");
 
-//             var params =
-//             {
-//                 credentialOfferParameters: {
-//                     definitionId: process.env.CRED_DEF_ID,
-//                     connectionId: req.body.object_id
-//                 }
-//             }
-//             await client.createCredential(params);
-//         }
-//         else if (req.body.message_type === 'credential_request') {
-//                                 console.log("cred request notif");
+            // var params =
+            // {
+            //     credentialOfferParameters: {
+            //         definitionId: process.env.CRED_DEF_ID,
+            //         connectionId: req.body.object_id
+            //     }
+            // }
+            // await client.createCredential(params);
+        }
+        else if (req.body.message_type === 'credential_request') {
+                                console.log("cred request notif");
 
-//             const attribs = cache.get(req.body.data.ConnectionId)
-//             if (attribs) {
-//                 var param_obj = JSON.parse(attribs);
-//                 const params = {
-//                     values: {
-//                         "Full Name": param_obj["name"],
-//                         "Title": param_obj["title"],
-//                         "Company Name": param_obj["org"],
-//                         "Phone Number": param_obj["phone"],
-//                         "Email": param_obj["email"]
-//                     }
-//                 }
-//                 await client.issueCredential(req.body.object_id, params);
-//             }
-//         }
-//     }
-//     catch (e) {
-//         console.log(e.message || e.toString());
-//     }
-// });
+            const attribs = cache.get(req.body.data.ConnectionId)
+            if (attribs) {
+                var param_obj = JSON.parse(attribs);
+                // const params = {
+                //     values: {
+                //         "Full Name": param_obj["name"],
+                //         "Title": param_obj["title"],
+                //         "Company Name": param_obj["org"],
+                //         "Phone Number": param_obj["phone"],
+                //         "Email": param_obj["email"]
+                //     }
+                // }
+                // await client.issueCredential(req.body.object_id, params);
+            }
+        }
+    }
+    catch (e) {
+        console.log(e.message || e.toString());
+    }
+});
 
 //FRONTEND ENDPOINT
 app.post('/api/issue', cors(), async function (req, res) {
@@ -180,7 +180,7 @@ var server = server.listen(PORT, async function () {
     console.log("============= \n\n" + url_val + "\n\n =========");
     var response = await client.createWebhook({
         webhookParameters: {
-            url: url_val + "/webhook",  // process.env.NGROK_URL
+            url: "http://45660c45.ngrok.io/webhook",  // process.env.NGROK_URL
             type: "Notification"
         }
     });
